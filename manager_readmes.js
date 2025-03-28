7b9916fa9adfc51cd1e70b18b0d09e8d3aa95f29
AndriiKot <AndriiKot@users.noreply.github.com>
2025-03-28 23:38:56 +0100

    'Manager projects: freeCodeCamp' ! **refactor: getNumberStep header leverl 4 testing 4 !

--- manager_readmes.js
+++ manager_readmes.js
@@ -30,7 +30,7 @@ function getLastFolderStep(folders) {
 
 function getNumberStep(folder) {
   console.log("TEST: getNumberStep", folder);
-  return `<h4></h4> Step  ${+folder.replace(/\D/g, "")}</h4>`;
+  return `<h4>Step  ${+folder.replace(/\D/g, "")}</h4>`;
 }
 
 function generateCodesProject() {
